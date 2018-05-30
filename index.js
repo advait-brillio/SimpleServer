18efe9e7b085b809558a39d64c1ecd72a1d3d0ac
SandeepAB1995 <39511694+SandeepAB1995@users.noreply.github.com>
2018-05-30 16:25:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
+
 var http = require('http');
-var handle = {};
-handle['/'] = home;
-handle['/authorize'] = authorize;
-handle['/code'] = code;
-handle['/mail;'] = mail;
-function route(handle, pathname, response, request) {
-  console.log('About to route a request for ' + pathname);
-  if (typeof handle[pathname] === 'function') {
-    return handle[pathname](response, request);
-  } else {
-    console.log('No request handler found for ' + pathname);
-    response.writeHead(404 ,{'Content-Type': 'text/plain'});
-    response.write('404 Not Found');
-    response.end();
-  }
-}
-start(route, handle);
-function start(route, handle) {
-  function onRequest(request, response) {
-    var pathName = url.parse(request.url).pathname;
-    console.log('Request for ' + pathName + ' received.');
-    route(handle, pathName, response, request);
-  }
-  
-  var port = 3000;
-  http.createServer(onRequest).listen(port);
-  console.log('Server has started. Listening on port: ' + port + '...');
-}
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+   //end the response
+}).listen(3000);
