440d9fd4016559cb0eee47cfc8458a0e612bf238
SandeepAB1995 <39511694+SandeepAB1995@users.noreply.github.com>
2018-05-30 15:46:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var http = require('http');
 
 var server = http.createServer(function(request, response) {
 
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.writeHead(200, {"Content-Type": "text/html"});
+  
 
 });
 
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 3000;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
